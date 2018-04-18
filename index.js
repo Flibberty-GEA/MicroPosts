69b22115577cd9f1d0f41888148576d8e618a3b9
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-04-18 22:28:18 +0300

    Добавил добавление нового поста Добавил класс для кнопки добавления

--- index.js
+++ index.js
@@ -3,11 +3,33 @@ import { http } from './src/http';
 import { ui } from './src/ui';
 
 // Get posts on DOM
-
 document.addEventListener('DOMContentLoaded', getPosts);
+
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
 
 function getPosts() {
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
     .catch(err => console.error(err))
+}
+
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  }
+
+  // Create Post
+  http.post('http://localhost:3000/posts', data)
+    .then(() => {
+      getPosts();
+    })
+    .catch(err => {
+      console.error(err)
+    });
+
 }
